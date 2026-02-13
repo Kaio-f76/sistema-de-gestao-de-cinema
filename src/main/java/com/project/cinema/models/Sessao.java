dbc505d9801704ace7c58e71d3851dcb0ec9583d
PedroGomes101 <pedrogomesdecarvalholopes11@gmail.com>
2026-02-12 22:22:50 -0300

    feat: Classes bases do sistema e seus relacionamentos.

---
+++ src/main/java/com/project/cinema/models/Sessao.java
@@ -0,0 +1,62 @@
+package com.project.cinema.models;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name="sessao")
+public class Sessao {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Temporal(TemporalType.DATE)
+    private Date date;
+
+    private String horario;
+
+    @ManyToOne
+    @JoinColumn(name = "sala_id")
+    private Salas sala;
+
+    @OneToMany(mappedBy = "ingresso", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Ingresso> ingresso = new ArrayList<>();
+    // getters e setters
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getHorario() {
+        return horario;
+    }
+
+    public void setHorario(String horario) {
+        this.horario = horario;
+    }
+
+    public Salas getSala() {
+        return sala;
+    }
+
+    public void setSala(Salas sala) {
+        this.sala = sala;
+    }
+}
