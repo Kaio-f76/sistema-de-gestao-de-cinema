a64a48dd72eb15ca6324a77d1a230865587910e7
Kaio <Kaiofabiogit@gmail.com>
2026-02-15 22:21:40 -0300

    feat: adiciona UsuarioController

---
+++ src/main/java/com/project/cinema/controllers/UsuarioController.java
@@ -0,0 +1,29 @@
+package com.project.cinema.controllers;
+
+import com.project.cinema.models.Usuario;
+import com.project.cinema.services.UsuarioService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/usuarios")
+public class UsuarioController {
+
+    private final UsuarioService usuarioService;
+
+    public UsuarioController(UsuarioService usuarioService) {
+        this.usuarioService = usuarioService;
+    }
+
+    @PostMapping("/cadastro")
+    public ResponseEntity<Usuario> criarConta(@RequestBody Usuario usuario) {
+        Usuario novoUsuario = usuarioService.criarConta(usuario);
+        return ResponseEntity.ok(novoUsuario);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<Usuario> login(@RequestParam String email, @RequestParam String senha) {
+        Usuario usuario = usuarioService.login(email, senha);
+        return ResponseEntity.ok(usuario);
+    }
+}
