47340bb710fe47ef9c27cdef99610733403dcaa8
Kaio <Kaiofabiogit@gmail.com>
2026-02-15 22:21:40 -0300

    feat: adiciona exceção EmailJaCadastradoException

---
+++ src/main/java/com/project/cinema/exceptions/EmailJaCadastradoException.java
@@ -0,0 +1,7 @@
+package com.project.cinema.exceptions;
+
+public class EmailJaCadastradoException extends RuntimeException {
+    public EmailJaCadastradoException(String message) {
+        super(message);
+    }
+}
