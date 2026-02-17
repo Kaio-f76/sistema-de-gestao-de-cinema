725b2ac985aad86a2cfd538b732bbe577e2f2790
Kaio <Kaiofabiogit@gmail.com>
2026-02-17 14:55:13 -0300

    feat: adiciona SecurityConfig.java

---
+++ src/main/java/com/project/cinema/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.project.cinema.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf().disable()
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/api/usuarios/cadastro").permitAll() // libera cadastro
+                .requestMatchers("/api/usuarios/login").permitAll()    // libera login
+                .anyRequest().authenticated()
+            )
+            .httpBasic(); // mantém basic auth para o resto
+        return http.build();
+    }
+}
