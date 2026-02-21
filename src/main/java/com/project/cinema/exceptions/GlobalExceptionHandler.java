e36ea9a2b2d6469c70477db476b968b973e0e26b
Kaio <Kaiofabiogit@gmail.com>
2026-02-21 17:00:13 -0300

    feat: adiciona GlobalExceptionHandler

---
+++ src/main/java/com/project/cinema/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,51 @@
+package com.project.cinema.exceptions;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+//Classe referente aos erros gerados
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(DadosInvalidosException.class)
+    public ResponseEntity<Object> handleDadosInvalidos(DadosInvalidosException ex) {
+
+        Map<String, Object> erro = new HashMap<>();
+        erro.put("timestamp", LocalDateTime.now());
+        erro.put("status", 400);
+        erro.put("error", "Bad Request");
+        erro.put("message", ex.getMessage());
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(erro);
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<Object> handleNotFound(EntityNotFoundException ex) {
+
+        Map<String, Object> erro = new HashMap<>();
+        erro.put("timestamp", LocalDateTime.now());
+        erro.put("status", 404);
+        erro.put("error", "Not Found");
+        erro.put("message", ex.getMessage());
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(erro);
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<Object> handleIllegalArgument(IllegalArgumentException ex) {
+
+        Map<String, Object> erro = new HashMap<>();
+        erro.put("timestamp", LocalDateTime.now());
+        erro.put("status", 400);
+        erro.put("error", "Bad Request");
+        erro.put("message", ex.getMessage());
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(erro);
+    }
+}
