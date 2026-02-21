5b64ea32164412f3966fc4bdcb0743c4b8130ec1
Kaio <Kaiofabiogit@gmail.com>
2026-02-21 17:00:13 -0300

    feat: ajustes no SecurityConfig

--- src/main/java/com/project/cinema/config/SecurityConfig.java
+++ src/main/java/com/project/cinema/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.project.cinema.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -19,26 +20,31 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-            .csrf(csrf -> csrf.disable())
-            .cors(Customizer.withDefaults()) // usa o CorsConfigurationSource abaixo
-            .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/api/usuarios/cadastro").permitAll()
-                .requestMatchers("/api/usuarios/login").permitAll()
-                .requestMatchers("/api/usuarios/logout").permitAll()
-                .requestMatchers("/api/usuarios/session-expired").permitAll()
-                .anyRequest().authenticated()
-            )
-            .httpBasic(Customizer.withDefaults())
-            .sessionManagement(session -> session
-                .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED) // só cria sessão se necessário
-                .maximumSessions(1)
-                .and()
-                .invalidSessionUrl("/api/usuarios/session-expired")
-            );
+                .csrf(csrf -> csrf.disable())
+                .cors(Customizer.withDefaults())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/usuarios/cadastro").permitAll()
+                        .requestMatchers("/api/usuarios/login").permitAll()
+                        .requestMatchers("/api/usuarios/logout").permitAll()
+                        .requestMatchers("/api/usuarios/session-expired").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/salas/**").permitAll()
+                        // Rotas de Administrador - hasRole("ADMINISTRADOR")
+                        .requestMatchers(HttpMethod.POST, "/api/salas/**").permitAll()
+                        .requestMatchers(HttpMethod.PUT, "/api/salas/{id}/{usuarioId}**").permitAll()
+                        .requestMatchers(HttpMethod.DELETE, "/api/salas/{id}/{usuarioId}**").permitAll()
+                        .anyRequest().permitAll()
+                )
+                //Customizer.withDefaults()
+                .httpBasic(basic -> basic.disable())
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
+                        .maximumSessions(1)
+                        .and()
+                        .invalidSessionUrl("/api/usuarios/session-expired")
+                );
 
         return http.build();
     }
-
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
