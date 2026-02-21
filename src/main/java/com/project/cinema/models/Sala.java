c6797f109defa8768696d8e120150d55ca998378
Kaio <Kaiofabiogit@gmail.com>
2026-02-21 17:00:13 -0300

    feat: adiciona Sala

---
+++ src/main/java/com/project/cinema/models/Sala.java
@@ -0,0 +1,62 @@
+package com.project.cinema.models;
+
+import jakarta.persistence.*;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.UUID;
+
+@Entity
+@Table(name = "salas")
+public class Sala {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String nome;
+
+    private Integer numAssentos;
+
+    @OneToMany(mappedBy = "sala", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Sessao> sessoes = new ArrayList<>();
+
+    public Sala() {}
+
+    public Sala(String nome, Integer numAssentos, List<Sessao> sessoes) {
+        this.nome = nome;
+        this.numAssentos = numAssentos;
+        this.sessoes = sessoes;
+    }
+    public UUID getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Integer getNumAssentos() {
+        return numAssentos;
+    }
+
+    public List<Sessao> getSessoes() {
+        return sessoes;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setNumAssentos(Integer numAssentos) {
+        this.numAssentos = numAssentos;
+    }
+
+    public void setSessoes(List<Sessao> sessoes) {
+        this.sessoes = sessoes;
+    }
+// getters e setters
+}
