912fc625e53e74f3eb79e77ee3ff4cae3bdd37cc
Kaio <Kaiofabiogit@gmail.com>
2026-03-02 11:25:35 -0300

    feat: atualizar UsuarioRepository.java

--- src/main/java/com/project/cinema/repositories/UsuarioRepository.java
+++ src/main/java/com/project/cinema/repositories/UsuarioRepository.java
@@ -1,6 +1,7 @@
 package com.project.cinema.repositories;
 
 import com.project.cinema.models.Usuario;
+import com.project.cinema.models.TipoUsuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -8,4 +9,6 @@ import java.util.UUID;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, UUID> {
     Optional<Usuario> findByEmail(String email);
+    Optional<Usuario> findByTipoUsuario(TipoUsuario tipoUsuario);
+
 }
